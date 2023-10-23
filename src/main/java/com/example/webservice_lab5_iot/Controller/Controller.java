88fc05234ff4cec29198ec3f68a508e3bf8f3b43
Jhondpc <a20196137@pucp.edu.pe>
2023-10-23 02:14:36 -0500

    aaa

--- src/main/java/com/example/webservice_lab5_iot/Controller/Controller.java
+++ src/main/java/com/example/webservice_lab5_iot/Controller/Controller.java
@@ -5,12 +5,15 @@ import com.example.webservice_lab5_iot.Repository.EmployeeRepository;
 import jakarta.persistence.Entity;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -25,5 +28,17 @@ public class Controller {
         return employeeRepository.listarTrabajadoresPorManager(managerId);
     }
 
+    //Buscar trabajador por id
+    @GetMapping("/tutor/trabajador")
+    public ResponseEntity<HashMap<String,Object>> obtenerTrabajador(@RequestParam("employeeId") int employeeId){
+        HashMap<String,Object> hashMap = new HashMap<>();
+        Optional<Employee> optionalEmployee = employeeRepository.findById(employeeId);
+        if(optionalEmployee.isPresent()){
+            hashMap.put("employee", optionalEmployee.get());
+        }else {
+            hashMap.put("msg","No existe un trabajador con ese ID");
+        }
+        return ResponseEntity.ok(hashMap);
+    }
 
 }
